f5b067b9849fcfb0948be80cb8f6ae221ce1771e
Ali Adineh <ahoora.ali@gmx.at>
2023-09-11 22:07:52 +0200

    small changes towards being responsive

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,40 +7,41 @@ const Home = () => {
   return (
     <div
       name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
+      className=" w-full pt-32 bg-gradient-to-b from-black via-black to-gray-800 flex "
     >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
-        <div className="flex flex-col justify-center h-full">
-          <h2 className="text-4xl sm:text-7xl font-bold text-white">
-            I'm a Fullstack JavaScript Developer
-          </h2>
-          <p className="text-gray-500 py-4 max-w-md">
-            Seit Januar 2023 sammle ich wertvolle Erfahrungen als
-            Softwareentwickler. Mein besonderes Interesse liegt derzeit in der
-            faszinierenden Welt der Webentwicklung. In dieser aufregenden Domäne
-            beschäftige ich mich intensiv mit React und Tailwind.
-          </p>
+      <div className="max-w-screen-lg mx-auto flex flex-col sm:flex-row items-center justify-center h-full px-4 ">
+        <div className="flex flex-col justify-center md:w-full md:flex-row">
           <div>
-            <Link
-              to="portfolio"
-              smooth
-              duration={500}
-              className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer "
-            >
-              Portfolio
-              <span className="group-hover:rotate-90 duration-300">
-                <MdKeyboardArrowRight size={25} className="ml-1 " />
-              </span>
-            </Link>
+            <h2 className="text-4xl sm:text-7xl font-bold text-white">
+              I'm a Fullstack JavaScript Developer
+            </h2>
+            <p className="text-gray-500 py-4 max-w-md">
+              Seit Januar 2023 sammle ich wertvolle Erfahrungen als
+              Softwareentwickler. Mein besonderes Interesse liegt derzeit in der
+              faszinierenden Welt der Webentwicklung. In dieser aufregenden
+              Domäne beschäftige ich mich intensiv mit React und Tailwind.
+            </p>
+            <div>
+              <Link
+                to="portfolio"
+                smooth
+                duration={500}
+                className="group text-white w-fit px-6 py-3 my-2 flex items-center rounded-md bg-gradient-to-r from-cyan-500 to-blue-500 cursor-pointer"
+              >
+                Portfolio
+                <span className="group-hover:rotate-90 duration-300">
+                  <MdKeyboardArrowRight size={25} className="ml-1" />
+                </span>
+              </Link>
+            </div>
           </div>
-        </div>
-
-        <div>
-          <img
-            src={myImage}
-            alt="my profile"
-            className="rounded-2xl mx-auto w-2/3 md:w-full "
-          />
+          <div>
+            <img
+              src={myImage}
+              alt="my profile"
+              className="rounded-2xl mx-auto w-2/3 md:w-full "
+            />
+          </div>
         </div>
       </div>
     </div>
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -6,61 +6,73 @@ import reactParallax from "../assets/portfolio/reactParallax.jpg";
 import reactSmooth from "../assets/portfolio/reactSmooth.jpg";
 import reactWeather from "../assets/portfolio/reactWeather.jpg";
 
-const Portfolio = () => {
-  const portfolios = [
-    {
-      id: 1,
-      src: arrayDestruct,
-    },
-    {
-      id: 2,
-      src: installNode,
-    },
-    {
-      id: 3,
-      src: navbar,
-    },
-    {
-      id: 4,
-      src: reactParallax,
-    },
-    {
-      id: 5,
-      src: reactSmooth,
-    },
-    {
-      id: 6,
-      src: reactWeather,
-    },
-  ];
+const portfolios = [
+  {
+    id: 1,
+    src: arrayDestruct,
+  },
+  {
+    id: 2,
+    src: installNode,
+  },
+  {
+    id: 3,
+    src: navbar,
+  },
+  {
+    id: 4,
+    src: reactParallax,
+  },
+  {
+    id: 5,
+    src: reactSmooth,
+  },
+  {
+    id: 6,
+    src: reactWeather,
+    demoLink: "https://aliadineh89.github.io/Weather-App/",
+    codeLink: "https://github.com/AliAdineh89/Weather-App",
+  },
+];
 
+const Portfolio = () => {
   return (
     <div
       name="portfolio"
       className="bg-gradient-to-b from-black to-gray-800 w-full text-white md:h-screen"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8 ">
+      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full">
+        <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 border-gray-500">
             Portfolio
           </p>
           <p className="py-6">Check out some of my work right here</p>
         </div>
-        <div className=" grid sm:grid-cols-2 md:grid-cols-3 gap-8 px-12 sm:px-0">
-          {portfolios.map(({ id, src }) => (
-            <div key={id} className=" shadow-md shadow-gray-600 rounded-lg">
+        <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 px-6 md:px-0">
+          {portfolios.map(({ id, src, demoLink, codeLink }) => (
+            <div key={id} className="shadow-md shadow-gray-600 rounded-lg">
               <img
                 src={src}
                 alt=""
-                className=" rounded-md duration-200 hover:scale-105"
+                className="rounded-md duration-200 hover:scale-105"
               />
-              <div className=" flex items-center justify-center">
-                <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+              <div className="flex items-center justify-center space-x-4 mt-4">
+                <a
+                  href={demoLink}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="w-1/2 px-6 py-3 duration-200 hover:scale-105 bg-blue-500 text-white rounded-md"
+                >
                   Demo
-                </button>
-                <button className=" w-1/2 px-6 py-3 m-4 duration-200 hover:scale-105">
+                </a>
+                <a
+                  href={codeLink}
+                  target="_blank"
+                  rel="noreferrer"
+                  className="w-1/2 px-6 py-3 duration-200 hover:scale-105 bg-gray-500 text-white rounded-md"
+                >
                   Code
-                </button>
+                </a>
               </div>
             </div>
           ))}
